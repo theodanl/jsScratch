ac0b1d821b4945ae4cb5ae067e510abc42e9e078
theodanl <romanmaslennikov.h4ppy@mail.ru>
2023-06-06 17:58:59 +0400

    type conversion

---
+++ type conversion/script.js
@@ -0,0 +1,27 @@
+// let value = true;
+
+// alert(typeof value);
+
+// value = String(value);
+
+// alert (typeof value);
+
+
+// alert(  '6' / '2'   )
+
+
+
+// let str = '123';
+
+// alert(typeof str);
+
+// let num = Number(str);
+
+// alert (typeof num);
+
+
+// логическое преобразование
+
+alert( Boolean('25'));
+
+alert( Boolean(0));
