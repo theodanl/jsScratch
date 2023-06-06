38e9a43466248e41581b27dcf170302bbe584690
theodanl <romanmaslennikov.h4ppy@mail.ru>
2023-06-06 17:17:29 +0400

    promt and confirm

---
+++ Взаимодействие alert confirm/script.js
@@ -0,0 +1,18 @@
+// РАБОТА prompt
+// let yrName = prompt('Назови своё имя', '');
+
+// alert(`Тебя зовут: ${yrName}`);
+
+
+
+// РАБОТА confirm
+
+// let isWork = confirm('Ты работаешь?');
+
+// alert(isWork);
+
+// ЗАДАЧКА С ВЫВОДОМ ИМЕНИ ПОЛЬЗОВАТЕЛЯ
+
+let userName = prompt('Введите имя пользователя', '');
+
+alert (`Твоё имя: ${userName}`);
