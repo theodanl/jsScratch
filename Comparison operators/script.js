1ed7a42cbca3800900f654d10d17a758b80b348a
theodanl <romanmaslennikov.h4ppy@mail.ru>
2023-06-07 17:21:59 +0400

    comparison operators

---
+++ Comparison operators/script.js
@@ -0,0 +1,30 @@
+// let result = 5==4;
+
+// alert(result);
+
+
+// alert( 'А' == 'А' );
+
+
+// alert ('2' == 2); 
+
+// alert ( null >= 0);
+
+
+// ЗАДАЧКИ
+
+5 > 4
+"ананас" > "яблоко"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+
+//true
+//false
+//true
+//true
+//false
+//false
+//false
