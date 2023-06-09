bdf1a78e074aee8a89236682fcefdbd862a11edc
theodanl <romanmaslennikov.h4ppy@mail.ru>
2023-06-09 12:46:01 +0400

    while,for,break,continue

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # jsScratch
 
 Learn the basics of java script from scratch "learn site js".
-The .docx .html files will not be sent as you solve typical textbook problems. Only the last commit will be with all files without limit.
+The .docx .html files will not be sent as you solve typical textbook problems.
---
+++ while, for/script.js
@@ -0,0 +1,93 @@
+// let i = 0;
+
+// while (i <= 3) {
+//     alert (i);
+
+//     i++;
+// }
+
+// let i = 3;
+
+// while (i ) {
+//     i--;
+
+//     alert(i);
+// }
+
+
+
+// ЕСЛИ ТЕЛО СОСТОИТ ИЗ ОДНОЙ ИНСТРУКЦИИ
+// let i = 3;
+
+// while(i) alert(i--);
+
+
+
+// let i = 0;
+
+// do {
+//     alert(i++);
+
+
+// }while (i <= 3);
+
+
+
+
+// for (let i = 0; i < 3; i++ ){         
+//     alert(i);
+// }
+
+// for (let i = 0; i < 3; i++) alert(i)
+
+// // Выполнить начало
+// let i = 0;
+// // Если условие == true → Выполнить тело, Выполнить шаг
+// if (i < 3) { alert(i); i++ }
+// // Если условие == true → Выполнить тело, Выполнить шаг
+// if (i < 3) { alert(i); i++ }
+// // Если условие == true → Выполнить тело, Выполнить шаг
+// if (i < 3) { alert(i); i++ }
+// // ...конец, потому что теперь i == 3
+
+
+
+// let i = 0; 
+
+// for ( ; i < 3;) {
+// //     alert(i);
+//         alert(i++);
+// }
+
+
+// let sum = 0;
+
+// while(true) {
+//     let value = +prompt("число");
+
+//     if (!value) break;
+
+//     sum += value;
+// }alert('сумма' + sum);
+
+
+// for (let i = 0; i <10 ; i++) {
+//     if (i% 2 == 0 ) continue;
+
+//     alert(i);
+// }
+
+
+
+// ВЫХОД ИЗ ЦИКЛА ПРИ ПОМОЩИ МЕТКИ(out)
+// out: for(let i=0 ; i<3 ; i++) {
+//     for (let j=0; j < 3 ; j++ ){
+//         let input = prompt(`введите координаты ${i},${j}`);
+
+//         if (!input) break out;
+//     }
+// }
+
+// alert('complete');
+
+
