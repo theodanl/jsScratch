f31cc831aba97e51b9d75041e66859fbd5bb6432
theodanl <romanmaslennikov.h4ppy@mail.ru>
2023-06-11 17:24:11 +0400

    arg-parametr

---
+++ function/script.js
@@ -0,0 +1,49 @@
+// function showMessage(){
+//     alert('hello tilted');
+// }
+
+// showMessage();
+
+
+// function showMessage(){
+//     let box = 'Привет я изучаю JS';
+
+//     alert(box);
+// }
+
+// showMessage();
+
+
+// let userName = 'your Login';
+
+// function showMessage() {
+//     let message = `Привет, ${userName}`;
+//     alert(message);
+// }
+
+// showMessage();
+
+
+// // parametri
+
+// let from = 'Рома';
+
+// function showMessage(from, text) {
+//     from = '*' + from + '*';
+//     alert(from + ': ' + text);
+// }
+
+// showMessage(from, "Привет");
+
+
+// function showMessage(text) {
+//     if (text === undefined ) {
+//         text = 'пустое сообщение'
+//     }
+//     alert(text);
+// }
+
+// showMessage();
+
+
+
