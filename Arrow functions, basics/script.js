ec3aa6dc53160fbc0ba5324fa95edddc1ac0f81e
theodanl <romanmaslennikov.h4ppy@mail.ru>
2023-06-28 13:01:58 +0400

    arrow functions, basic

---
+++ Arrow functions, basics/script.js
@@ -0,0 +1,47 @@
+// let sum = (a,b,c) => a+b+c;
+
+// a = +prompt('Введите число a');
+// b = +prompt('Введите число b');
+// c = +prompt('Введите число c');
+
+// alert(sum(a,b,c));
+
+// let double = n => n * +prompt('Введите число для умножения N');
+
+// alert(double(3));
+
+
+// let age = +prompt('Сколько вам лет?');
+
+// let welcome = (age < 18) ?
+
+// () => alert('Привет') : 
+// () => alert('Тебе много лет');
+
+// welcome();
+
+
+// let sum = (a,b) => {
+//     a = +prompt('введи а');
+//     b = +prompt('введи b');
+//     let result = a+b;
+//     return result;
+// };
+
+// alert(sum (1,2));
+
+
+// tasks
+
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+
+// ask( 
+//     "Вы согласны?", 
+//     () => alert('Вы согласились'),
+//     () => alert('Вы отменили')
+// );
+
